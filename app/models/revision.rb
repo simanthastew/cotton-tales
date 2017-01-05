d38ee4748a70e007f2f0b53fdca32f3986a56ed2
emgreen33 <greenemily33@gmail.com>
2017-01-05 17:30:02 -0600

    Write logged out author generator method

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -4,4 +4,16 @@ class Revision < ActiveRecord::Base
 
   validates :body, presence: true
   validates :author, presence: true
+  before_validation :set_guest
+
+  private
+    def set_guest
+      if self.author.nil?
+        self.author = User.create(username: generate_user, password: "password")
+      end
+    end
+
+    def generate_user
+      "Guest-" + SecureRandom.hex(3)
+    end
 end
