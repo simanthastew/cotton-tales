25d084d56a3d596bd8278ae3761e99e471b9287b
emgreen33 <greenemily33@gmail.com>
2017-01-05 11:21:43 -0600

    Get tests running green for features

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 feature "visiting the categories page" do
   scenario "the user sees all the categories" do
     category = Category.create(:name => "Cute")
-  # as a user i want to see all the categories on the categories page
     visit('/categories')
     expect(page).to have_content 'Cute'
   end
@@ -11,11 +10,8 @@ end
 
 feature "visiting the categories page" do
   scenario "the user sees all the articles under a category" do
-      #given a set posts in the db
     category = Category.create(:name => "Cute")
-  # as a user i want to visit the category page
     visit("/categories/#{category.id}")
-  # so that I can see the title of the page
     expect(page).to have_content 'Cute'
     expect(page).to have_content 'All Categories'
   end
@@ -23,13 +19,11 @@ end
 
 feature "visiting the categories page" do
   scenario "the user sees all the articles under a category" do
-    mock_model Article, {:title => "Cool article", :body => "body!"}
     category = Category.create(:name => "Cute")
-    category.articles.create()
-  # as a user i want to visit the category page
+    user = User.create(username: "emily", password: "password")
+    category.articles.create!(title: "Sweeet", body: "body test", author_id: user.id)
     visit("/categories/#{category.id}")
-  # so that I can see the list of articles on the page
-
+    expect(page).to have_content "Sweeet"
     expect(page).to have_content 'All Categories'
   end
 end
