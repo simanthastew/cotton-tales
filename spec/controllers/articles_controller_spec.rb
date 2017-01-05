bf2d4ebb750d3817a83261d68dca4f9f922c9a41
emgreen33 <greenemily33@gmail.com>
2017-01-05 16:21:31 -0600

    Uncommented tests

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,107 +1,107 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe ArticlesController do
-#   let(:sample_article) { Article.create(title: "test", body: "test body", author_id: 1) }
+describe ArticlesController do
+  let(:sample_article) { Article.create!(title: "test", body: "test body", author_id: 1) }
 
-#   describe "GET #index" do
-#     before(:each) do
-#       get :index
+  describe "GET #index" do
+    before(:each) do
+      get :index
+    end
+    it "responds with status code 200" do
+      expect(response).to have_http_status 200
+    end
+    it "assigns all the articles to @articles" do
+      expect(assigns(:articles)).to eq(Article.all)
+    end
+    it "renders the :index template" do
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe "GET #show" do
+    before(:each) do
+      get :show, {id: sample_article.id}
+    end
+
+    it "responds with status 200" do
+      expect(response).to have_http_status 200
+    end
+
+    it "assigns the correct article as @article" do
+      expect(assigns(:article)).to eq(sample_article)
+    end
+
+    it "renders the show template" do
+      expect(response).to render_template(:show)
+    end
+  end
+
+  describe "GET #edit" do
+    before(:each) do
+      get :edit, {id: sample_article.id}
+    end
+
+    it "responds with status 200" do
+      expect(response).to have_http_status 200
+    end
+    it "assigns the correct article as @article" do
+      expect(assigns(:article)).to eq(sample_article)
+    end
+    it "renders the edit template" do
+      expect(response).to render_template(:edit)
+    end
+  end
+
+  describe "GET #new" do
+    before(:each) do
+      get :new
+    end
+
+    it "responds with status 200" do
+      expect(response).to have_http_status 200
+    end
+
+    it "renders the new template" do
+      expect(response).to render_template(:new)
+    end
+  end
+
+# commenting out these tests as they still fail. Will update with new PR.
+#   describe "POST #create" do
+#     context "when valid parameters are passed" do
+#       before(:each) do
+#         post :create, { article: {title: "Test", body: "test body", author_id: 1}}
+#       end
+#       it "responds with status 302" do
+#         expect(response).to have_http_status 302
+#       end
+#       it "creates a new article in the database" do
+#         expect(article.save).to be true
+#       end
+#       it "assigns the newly created article as @article" do
+#         expect(assigns(:article)).to eq(Article.last)
+#       end
+#       it "redirects to the newly created article" do
+#         expect(response).to redirect_to Article.last
+#       end
 #     end
-#     it "responds with status code 200" do
-#       expect(response).to have_http_status 200
-#     end
-#     it "assigns all the articles to @articles" do
-#       expect(assigns(:articles)).to eq(Article.all)
-#     end
-#     it "renders the :index template" do
-#       expect(response).to render_template(:index)
+#     context "when invalid parameters are passed" do
+#       before(:each) do
+#         count = Article.count
+#         post :create, { article: {body: "test body", author_id: 1}}
+#       end
+#       it "responds with status 422: Unprocessable Entity" do
+#         expect(response).to have_http_status 422
+#       end
+#       it "does not create a new article in the database" do
+#         expect(Article.count).to eq count
+#       end
+#       it "assigns the unsaved article as @article" do
+#         expect(assigns(:article)).to be_a_new Article
+#       end
+#       it "renders the #new template" do
+#         expect(response).to render_template :new
+#       end
 #     end
 #   end
-
-#   describe "GET #show" do
-#     before(:each) do
-#       get :show, {id: sample_article.id}
-#     end
-
-#     it "responds with status 200" do
-#       expect(response).to have_http_status 200
-#     end
-
-#     it "assigns the correct article as @article" do
-#       expect(assigns(:article)).to eq(sample_article)
-#     end
-
-#     it "renders the show template" do
-#       expect(response).to render_template(:show)
-#     end
-#   end
-
-#   describe "GET #edit" do
-#     before(:each) do
-#       get :edit, {id: sample_article.id}
-#     end
-
-#     it "responds with status 200" do
-#       expect(response).to have_http_status 200
-#     end
-#     it "assigns the correct article as @article" do
-#       expect(assigns(:article)).to eq(sample_article)
-#     end
-#     it "renders the edit template" do
-#       expect(response).to render_template(:edit)
-#     end
-#   end
-
-#   describe "GET #new" do
-#     before(:each) do
-#       get :new
-#     end
-
-#     it "responds with status 200" do
-#       expect(response).to have_http_status 200
-#     end
-
-#     it "renders the new template" do
-#       expect(response).to render_template(:new)
-#     end
-#   end
-
-# # commenting out these tests as they still fail. Will update with new PR.
-# #   describe "POST #create" do
-# #     context "when valid parameters are passed" do
-# #       before(:each) do
-# #         post :create, { article: {title: "Test", body: "test body", author_id: 1}}
-# #       end
-# #       it "responds with status 302" do
-# #         expect(response).to have_http_status 302
-# #       end
-# #       it "creates a new article in the database" do
-# #         expect(article.save).to be true
-# #       end
-# #       it "assigns the newly created article as @article" do
-# #         expect(assigns(:article)).to eq(Article.last)
-# #       end
-# #       it "redirects to the newly created article" do
-# #         expect(response).to redirect_to Article.last
-# #       end
-# #     end
-# #     context "when invalid parameters are passed" do
-# #       before(:each) do
-# #         count = Article.count
-# #         post :create, { article: {body: "test body", author_id: 1}}
-# #       end
-# #       it "responds with status 422: Unprocessable Entity" do
-# #         expect(response).to have_http_status 422
-# #       end
-# #       it "does not create a new article in the database" do
-# #         expect(Article.count).to eq count
-# #       end
-# #       it "assigns the unsaved article as @article" do
-# #         expect(assigns(:article)).to be_a_new Article
-# #       end
-# #       it "renders the #new template" do
-# #         expect(response).to render_template :new
-# #       end
-# #     end
-# #   end
-# end
+end
