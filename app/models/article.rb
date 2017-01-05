0f58c314f7db2aa6d04d5b3680f31b294ac7dc79
Noah Guy <email": null>
2017-01-05 10:28:23 -0600

    add Article model, migration and seed

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ActiveRecord::Base
+  belongs_to :author, class_name: "User"
+  has_many :categorizations
+  has_many :categories, through: :categorizations
+  has_many :revisions
+
+  validates :title, presence: true, uniqueness: true
+end
