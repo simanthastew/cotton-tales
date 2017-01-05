2ec64d672f908ca4dffd10696c30a81a1c54fe41
Noah Guy <email": null>
2017-01-05 11:22:25 -0600

    Create feature tests for articles
    https://github.com/TylerSmall19/cotton-tales
    
    As a User, I can create and edit an article.  I can also view all articles.
    
    Added feature tests for edit article page
    Added feature tests for showing a specific article
    Added feature tests for viewing all articles index.
    Tests are created, but DO NOT PASS. future commits will resolve failing tests.

---
+++ spec/features/visiting_articles_index_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+feature "visiting the article index page" do
+  scenario "The user can visit the page that lists all the articles" do
+    visit "/articles/index"
+    expect(page).to have_content "All Articles"
+  end
+end
