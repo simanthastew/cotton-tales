6374e186176b033485b15ddaa22ba5a12557eb27
emgreen33 <greenemily33@gmail.com>
2017-01-06 00:17:31 -0600

    Add in routes and basic controller function for revisions

---
+++ app/controllers/revisions_controller.rb
@@ -0,0 +1,17 @@
+class RevisionsController < ApplicationController
+# GET /articles/:article_id/revisions
+  def index
+    @revisions = Revision.where(article_id: params[:article_id])
+  end
+# GET /articles/:article_id/revisions/:id
+  def show
+    @revision = Revision.where(article_id: params[:article_id], id: params[:id])
+  end
+# GET /articles/:article_id/revisions/new
+  def new
+    @revision = Revision.new
+  end
+# POST /articles/:article_id/revisions
+  def create
+  end
+end
