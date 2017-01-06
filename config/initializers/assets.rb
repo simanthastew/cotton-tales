4396cde5a077066df5e67abbfb7bb27091ad4e1e
Tyler Small <email": null>
2017-01-06 16:41:43 -0600

    Fix Heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,8 +6,11 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
+Rails.application.config.assets.precompile += Ckeditor.assets
 Rails.application.config.assets.precompile += %w(ckeditor/config.js)
 Rails.application.config.assets.precompile += %w( ckeditor/* )
+Rails.application.config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
