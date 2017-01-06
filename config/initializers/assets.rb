39ebde8d93ffb1e3f98de9c787b36c862873ae89
Tyler Small <email": null>
2017-01-06 16:55:44 -0600

    Fix  heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += Ckeditor.assets
 Rails.application.config.assets.precompile += %w(ckeditor/config.js)
 Rails.application.config.assets.precompile += %w( ckeditor/* )
-Rails.application.config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+
 
 
 # Precompile additional assets.
