cb9b2d1be4d5effa5c90122ec2cd9bd9d81861c1
Emily Green <greenemily33@gmail.com>
2017-01-05 18:19:49 -0600

    Revisions branch (#25)
    * Add if statements to the categories index and show views
    
    * write view specs for category show.html.erb
    
    * Add css class for category feature test to pass
    
    * Write tests for categories show page
    
    * Have passing tests for show and index category views
    
    * Uncommented tests
    
    * Create revisions migration and model
    
    * Add user associations
    
    * Write logged out author generator method
    
    * Write model tests for revision model

--- spec/models/revision_spec.rb
+++ spec/models/revision_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Revision, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:author) { User.create(username: "barry", password: "password")}
+  let(:category) {Category.create(name: "All")}
+  let(:article) {Article.create(title: "Test Title", body: "test body", )}
+  let(:revision) { Revision.create(body: "test body", article_id: article.id, author_id: author.id) }
+  let(:revision_two) { Revision.new(body: "test body", article_id: article.id) }
+  describe Revision do
+    it "should have a body" do
+      expect(revision.body).to eq("test body")
+    end
+    it "should have belong to an article" do
+      expect(revision.article_id).to eq(article.id)
+    end
+    it "should have belong to an author" do
+      expect(revision.author_id).to eq(author.id)
+    end
+    it "should be created with a random author" do
+      count = Revision.all.count
+      revision_two.save
+      expect(Revision.count).to eq(count+1)
+    end
+  end
 end
